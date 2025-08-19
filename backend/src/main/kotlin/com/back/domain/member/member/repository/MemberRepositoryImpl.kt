5c706c332aa884106bb2772b5612a6dd23404a59
meohin <meohin.meo@gmail.com>
2025-08-19 11:15:26 +0900

    feat: findByNicknameContaining 메서드를 QueryDSL 버전으로 구현

--- backend/src/main/kotlin/com/back/domain/member/member/repository/MemberRepositoryImpl.kt
+++ backend/src/main/kotlin/com/back/domain/member/member/repository/MemberRepositoryImpl.kt
@@ -75,4 +75,13 @@ class MemberRepositoryImpl(
             )
             .fetch()
     }
+
+    override fun findQByNicknameContaining(nickname: String): List<Member> {
+        val member = QMember.member
+
+        return queryFactory
+            .selectFrom(member)
+            .where(member.nickname.contains(nickname))
+            .fetch()
+    }
 }
