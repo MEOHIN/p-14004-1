c07fa82eedf9e653712edbf9b04d646287e629dd
meohin <meohin.meo@gmail.com>
2025-08-20 15:52:42 +0900

    feat: PostRepositoryImpl::findQPagedByKw 구현

---
+++ backend/src/main/kotlin/com/back/standard/dto/PostSearchSortType1.kt
@@ -0,0 +1,24 @@
+package com.back.standard.dto
+
+import com.back.standard.extensions.toCamelCase
+import org.springframework.data.domain.Sort
+
+enum class PostSearchSortType1 {
+    ID,
+    ID_ASC;
+
+    val sortBy by lazy {
+        Sort.by(
+            if (isAsc) Sort.Direction.ASC else Sort.Direction.DESC,
+            property
+        )
+    }
+
+    val property by lazy {
+        name.removeSuffix("_ASC").toCamelCase()
+    }
+
+    val isAsc by lazy {
+        name.endsWith("_ASC")
+    }
+}
